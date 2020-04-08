e151cfeaa483e72f2807fe7c340a4b5f8a4e8867
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-08 13:35:45 +0100

    ready to test new class implmentation of BM25 search

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from retrieval.tools import Search
 if __name__ == '__main__':
     index_path = '/nfs/trec_car/index/anserini_pages/lucene-index.car17v2.0.pages.anserini.full_index.v1'
     topics_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical.topics'
-    run_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_100_pyserini_lightweight_processing_new_params.run'
+    run_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_100_pyserini_class.run'
     hits = 100
     searcher_settings = {
         'BM25':
