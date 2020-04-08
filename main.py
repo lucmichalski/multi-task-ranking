2cf5669dea8c022f8528dfb7e6362f1a431fc9d0
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-08 15:22:56 +0100

    finished eval class - ready to test

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 
 
-from retrieval.tools import Search
+from retrieval.tools import Search, Eval
 
 if __name__ == '__main__':
     index_path = '/nfs/trec_car/index/anserini_pages/lucene-index.car17v2.0.pages.anserini.full_index.v1'
     topics_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical.topics'
-    run_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_pyserini_10.run'
+    qrels_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical.qrels'
+    run_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_10_pyserini.run'
     hits = 10
-    searcher_settings = {
-        'BM25':
-            {
-                'k1': 0.9,
-                'b': 0.4
-            }
+    searcher_config = {
+        'BM25': {'k1': 0.9, 'b': 0.4}
     }
-    search = Search(index_path=index_path, searcher_settings=searcher_settings)
-
+    search = Search(index_path=index_path, searcher_config=searcher_config)
     search.write_run_from_topics(topics_path=topics_path, run_path=run_path, hits=hits)
+
+    eval_config = {
+        'map': {'k': None},
+        'r_prec': {'k': None},
+        'recip_rank': {'k': None},
+        'precision': {'k': 20},
+        'recall': {'k': 40},
+        'ndcg': {'k': 20},
+    }
+    eval = Eval()
+    eval.write_eval_from_qrels_and_run(run_path=run_path, qrels_path=qrels_path, eval_config=eval_config)
+
+
--- retrieval/tools.py
+++ retrieval/tools.py
@@ -9,42 +9,55 @@ import re
 import os
 
 
+###############################################################################
+############################## Search Class ###################################
+###############################################################################
+
 
 class Search:
-    """  """
 
+    """ Information retrieval search class using Pyserini. """
+
+    # Pyserini implemented searcher configs.
     implemented_searchers = ['BM25', 'BM25+RM3']
 
-    def __init__(self, index_path, searcher_settings=None):
+
+    def __init__(self, index_path, searcher_config=None):
 
         # Initialise absolute path to Anserini (Lucene) index.
+        print("Index path: {}".format(index_path))
         self.index_path = index_path
         # Initialise index_utils for accessing index information.
         self.index_utils = pyutils.IndexReaderUtils(self.index_path)
-        # Initialise searcher configuration with searcher_settings dict. If no settings -> use SimpleSearcher.
-        self.searcher = self.__build_searcher(searcher_settings=searcher_settings)
+        # Initialise searcher configuration with searcher_config dict. If no settings -> use SimpleSearcher.
+        self.searcher = self.__build_searcher(searcher_config=searcher_config)
 
 
-    def __build_searcher(self, searcher_settings):
+    def __build_searcher(self, searcher_config):
+        """ Build Pyserini SimpleSearcher based on config."""
         searcher = pysearch.SimpleSearcher(index_dir=self.index_path)
-        if isinstance(searcher_settings, dict):
-            # BM25 configuration.
-            if 'BM25' in searcher_settings:
-                print('Using BM25 search model:\n{}'.format(searcher_settings))
-                searcher.set_bm25_similarity(b=searcher_settings['BM25']['b'], k1=searcher_settings['BM25']['k1'])
-            # BM25 with RM3 query expansion configuration.
-            elif 'BM25+RM3' in searcher_settings:
-                print('Using BM25+RM3 search model:\n{}'.format(searcher_settings))
-                searcher.set_bm25_similarity(b=searcher_settings['BM25+RM3']['BM25']['b'],
-                                             k1=searcher_settings['BM25+RM3']['BM25']['k1'])
-                searcher.set_rm3_reranker(fb_terms=searcher_settings['BM25+RM3']['RM3']['fb_terms'],
-                                          fb_docs=searcher_settings['BM25+RM3']['RM3']['fb_docs'],
-                                          original_query_weight=searcher_settings['BM25+RM3']['RM3']['original_query_weight'])
-            else:
-                print('NOT VALID searcher_settings --> will use simple search:\n{}'.format(searcher_settings))
-                raise
+        if isinstance(searcher_config, dict):
+            # Check valid searcher config
+            for k in searcher_config.keys():
+                assert k in self.implemented_searchers
 
+            # BM25 configuration.
+            if 'BM25' in searcher_config:
+                print('Using BM25 search model: {}'.format(searcher_config))
+                searcher.set_bm25_similarity(b=searcher_config['BM25']['b'], k1=searcher_config['BM25']['k1'])
+            # BM25 with RM3 query expansion configuration.
+            elif 'BM25+RM3' in searcher_config:
+                print('Using BM25+RM3 search model: {}'.format(searcher_config))
+                searcher.set_bm25_similarity(b=searcher_config['BM25+RM3']['BM25']['b'],
+                                             k1=searcher_config['BM25+RM3']['BM25']['k1'])
+                searcher.set_rm3_reranker(fb_terms=searcher_config['BM25+RM3']['RM3']['fb_terms'],
+                                          fb_docs=searcher_config['BM25+RM3']['RM3']['fb_docs'],
+                                          original_query_weight=searcher_config['BM25+RM3']['RM3']['original_query_weight'])
+            else:
+                print('NOT VALID searcher_settings --> will use simple search: {}'.format(searcher_config))
+                raise
             return searcher
+
         else:
             print('Using SimpleSearcher default')
             return searcher
@@ -115,21 +128,31 @@ class Search:
         print("Completed run - written to run file: {}".format(run_path))
 
 
+###############################################################################
+################################ Eval Class ###################################
+###############################################################################
+
+
 class Eval:
 
+    """ Information retrieval eval class. """
+
+    # Implemented eval metrics.
     def __init__(self):
         """  """
         self.implemented_metrics = {
             'map':          self.get_map,
-            'r_prec':       self.get_R_prec,
+            'r_prec':       self.get_Rprec,
             'recip_rank':   self.get_recip_rank,
-            'precision':    self.get_precision,
+            'precision':    self.get_P,
             'recall':       self.get_recall,
             'ndcg':         self.get_ndcg
         }
 
-    def get_map(self, run, R):
+    def get_map(self, run, R, k=None):
         """ Calculate mean average precision (MAP). """
+        if k != None:
+            run = run[:k]
         # Sum relevant docs in run.
         R_run = sum(run)
         # If relevant docs in run and total relevant docs above 0.
@@ -139,15 +162,17 @@ class Eval:
             # Loop of run and append precision to precision counter.
             for i, r in enumerate(run):
                 if r == 1.0:
-                    precision_sum += self.get_precision(run=run, k=(i + 1))
+                    precision_sum += self.get_P(run=run, k=(i + 1))
             # Divide precision counter by total relevant docs.
             return precision_sum / R
         else:
             return 0.0
 
 
-    def get_R_prec(self, run, R):
+    def get_Rprec(self, run, R, k=None):
         """ Calculate R-precision. """
+        if k != None:
+            run = run[:k]
         if R > 0:
             # Reduce run at index #R and calculate precision.
             return sum(run[:R]) / R
@@ -155,8 +180,10 @@ class Eval:
             return 0.0
 
 
-    def get_recip_rank(self, run):
+    def get_recip_rank(self, run, R=None, k=None):
         """ Calculate reciprocal rank. """
+        if k != None:
+            run = run[:k]
         for i, r in enumerate(run):
             # Return 1 / rank for first relevant.
             if r == 1.0:
@@ -164,16 +191,18 @@ class Eval:
         return 0.0
 
 
-    def get_precision(self, run, k=20):
-        """ Calculate precision at kth rank. """
-        run_k = run[:k]
-        return sum(run_k) / k
+    def get_P(self, run, R=None, k=20):
+        """ Calculate precision (P) at kth rank. """
+        if k != None:
+            run = run[:k]
+        return sum(run) / k
 
 
     def get_recall(self, run, R, k=40):
         """ Calculate recall at kth rank """
-        run_k = run[:k]
-        R_run = sum(run_k)
+        if k != None:
+            run = run[:k]
+        R_run = sum(run)
         if R > 0:
             return R_run / R
         return 1.0
@@ -181,14 +210,15 @@ class Eval:
 
     def get_ndcg(self, run, R, k=20):
         """ Calculate normalised discount cumulative gain (NDCG) at kth rank. """
-        run_k = run[:k]
+        if k != None:
+            run = run[:k]
         # Initialise discount cumulative gain.
         dcg = 0
         # Initialise perfect discount cumulative gain.
         i_dcg = 0
         R_run = sum(run)
         if (R_run > 0) and (R > 0):
-            for i, r in enumerate(run_k):
+            for i, r in enumerate(run):
                 if i == 0:
                     if (i + 1) <= R:
                         i_dcg += 1
@@ -219,25 +249,39 @@ class Eval:
         return qrels_dict
 
 
-    def get_query_metrics(self, run, R):
-        run_metrics = []
-        run_metrics.append(self.get_map(run=run, R=R))
-        run_metrics.append(self.get_R_prec(run=run, R=R))
-        run_metrics.append(self.get_recip_rank(run=run))
-        run_metrics.append(self.get_precision(run=run, k=20))
-        run_metrics.append(self.get_recall(run=run, k=40, R=R))
-        run_metrics.append(self.get_ndcg(run=run, R=R, k=20))
+    def get_query_metrics(self, run, R, eval_config):
+        """ Build metrics from eval_config. """
 
-        return run_metrics
+        # Assert a valid eval metric config.
+        assert isinstance(eval_config, dict)
+        for k in eval_config.keys():
+            assert k in self.implemented_metrics
 
-    def write_eval_from_qrels_and_run(self, run_path, qrels_path):
-        """ """
+        # Loop over implemented_metrics/eval_config.
+        query_metrics = ''
+        for m in self.implemented_metrics:
+            if m in eval_config:
+                # Build label of metric.
+                if eval_config[m]['k'] == None:
+                    metric_label = m
+                else:
+                    metric_label = m + '_' + str(eval_config[m]['k'])
+                # Calculate metric.
+                metric = self.implemented_metrics[m](run=run, k=eval_config[m]['k'], R=R)
+                # Append metric label and metric to string.
+                query_metrics += metric_label + ' ' + str(metric) + ' '
+
+        return query_metrics
+
+
+    def write_eval_from_qrels_and_run(self, run_path, qrels_path, eval_config):
+        """ TODO """
         # Eval path assumed to be run path with eval identifier added.
-        eval_path = run_path + '.eval.by_query'
-        #
+        eval_by_query_path = run_path + '.eval.by_query'
+        # build qrels dict {query: [rel#1, rel#2, etc.]}.
         qrels_dict = self.get_qrels_dict(qrels_path=qrels_path)
 
-        with open(eval_path, 'w') as f_eval:
+        with open(eval_by_query_path, 'w') as f_eval_by_query:
             with open(run_path, 'r') as f_run:
                 # Store query of run (i.e. previous query).
                 run_query = None
@@ -259,16 +303,56 @@ class Eval:
                                 run.append(1)
                             else:
                                 run.append(0)
+
                         # Calculate number of relevant docs in qrels (R).
                         R = len(qrels_dict[run_query])
-                        metrics = self.get_query_metrics(run=run, R=R)
-                        f_eval.write(str(metrics) + '\n')
+                        # Build query metric string.
+                        query_metrics = self.get_query_metrics(run=run, R=R, eval_config=eval_config)
+                        # Write query metric string to file.
+                        f_eval_by_query.write(query + ' ' + query_metrics + '\n')
+                        # Start next query.
                         run_doc_ids = []
                         run = []
 
+                    # Add doc_id to run and map run_query->query.
                     run_doc_ids.append(doc_id)
                     run_query = query
-                    print(query, doc_id)
+
+        # Store sum of metrics across all queries.
+        eval_metric_sum = {}
+        # Store number of queries - used to divide sum to find mean metric.
+        query_counter = 0
+        with open(eval_by_query_path, 'r') as f_eval_by_query:
+            for query_metrics in f_eval_by_query:
+                data = query_metrics.split()
+                # For index and data item in query metric.
+                for i, d in enumerate(data):
+                    # If string of implemented_metrics is substring of data item --> update eval dict.
+                    for k in self.implemented_metrics.keys():
+                        if k in d:
+                            if k not in eval_metric_sum:
+                                eval_metric_sum[d] = 0
+                            # Update
+                            eval_metric_sum[d] += float(data[i+1])
+                            break
+                # Update counter.
+                query_counter += 1
+
+        # Find mean of metrics.
+        eval_metric = {k: v / query_counter for k, v in eval_metric_sum.items()}
+
+        # Write overall eval to file.
+        eval_path = run_path + '.eval'
+        with open(eval_path, 'w') as f_eval:
+            for k, v in eval_metric.items():
+                f_eval.write(k + ' ' + str(v) + '\n')
+
+
+
+
+###############################################################################
+############################# Pipeline Class ##################################
+###############################################################################
 
 
 if __name__ == '__main__':
@@ -282,8 +366,16 @@ if __name__ == '__main__':
 
     # search = Search()
     # search.write_run_from_topics(index_path, topics_path, run_path, hits)
+    eval_config = {
+        'map': {'k': None},
+        'r_prec': {'k': None},
+        'recip_rank': {'k': None},
+        'precision': {'k': 20},
+        'recall': {'k': 40},
+        'ndcg': {'k': 20},
+    }
 
-    # eval = Eval()
-    # eval.write_eval_from_qrels_and_run(run_path, qrels_path)
+    eval = Eval()
+    eval.write_eval_from_qrels_and_run(run_path=run_path, qrels_path=qrels_path, eval_config=eval_config)
 
 
