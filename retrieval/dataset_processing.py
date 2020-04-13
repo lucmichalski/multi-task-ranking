ebb253431fcf090b94e971075b0f97a0480873ab
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-13 21:59:15 +0100

    added training dataset functionality

--- retrieval/dataset_processing.py
+++ retrieval/dataset_processing.py
@@ -4,6 +4,7 @@ from transformers import BertTokenizer
 from torch.utils.data import TensorDataset
 
 import collections
+import random
 import torch
 import time
 import six
@@ -15,6 +16,7 @@ class TrecCarProcessing:
 
     def __init__(self, qrels_path, run_path, index_path, data_dir_path,
                  tokenizer=BertTokenizer.from_pretrained('bert-base-uncased'), max_length=512):
+
         # Path to qrels file.
         self.qrels_path = qrels_path
         # Path to run file.
@@ -23,19 +25,32 @@ class TrecCarProcessing:
         self.index_path = index_path
         # Path to data directory to write output PyTorch file(s).
         self.data_dir_path = data_dir_path
+
         # Initialise searching capabilities over Anserini/Lucene index.
         self.search_tools = SearchTools(index_path=self.index_path)
+
         # Tokenizer function (text -> BERT tokens)
         self.tokenizer = tokenizer
+
         # Max length of BERT tokens.
         self.max_length = max_length
+
         # load qrels dictionary {query: [doc_id, doc_id, etc.]} into memory.
         self.qrels = self.get_qrels()
-        # Lists of BERT inputs
+
+        # Lists of BERT inputs.
         self.input_ids_list = []
         self.token_type_ids_list = []
         self.attention_mask_list = []
         self.labels_list = []
+
+        # Lists used to build BERT inputs.
+        # Stores data for sequential dataset
+        self.topic_BERT_encodings = []
+        # Having data in relevant (R) and not relevant (N) allows to added extra R samples.
+        self.topic_R_BERT_encodings = []
+        self.topic_N_BERT_encodings = []
+
         # Counter of current chuck being processed.
         self.chuck_counter = None
         # Count number of topics being processed.
@@ -44,7 +59,7 @@ class TrecCarProcessing:
         self.chuck_topic_size = None
 
 
-    def __convert_to_unicode(text):
+    def __convert_to_unicode(self, text):
         """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
         if six.PY3:
             if isinstance(text, str):
@@ -70,9 +85,9 @@ class TrecCarProcessing:
         return qrels
 
 
-    def __process_sequential_topic(self, topic_BERT_encodings):
-        """ Process sequentially and do not even classes through sampling. Used for validation dataset. """
-        for query, doc_id, BERT_encodings in topic_BERT_encodings:
+    def __process_sequential_topic(self):
+        """ Process sequentially and do not even classes through sampling. Used for building validation dataset. """
+        for query, doc_id, BERT_encodings in self.topic_BERT_encodings:
             # Append BERT model inputs.
             self.input_ids_list.append(BERT_encodings['input_ids'])
             self.token_type_ids_list.append(BERT_encodings['token_type_ids'])
@@ -83,21 +98,59 @@ class TrecCarProcessing:
             else:
                 self.labels_list.append([0])
 
+        # New topics.
+        self.topic_BERT_encodings = []
+        self.topic_R_BERT_encodings = []
+        self.topic_N_BERT_encodings = []
 
-    def __process_non_sequential_topic(self, topic_R_BERT_encodings, topic_N_BERT_encodings):
-        """ """
-        pass
+
+    def __process_non_sequential_topic(self):
+        """ Process sequentially and even classes through sampling. Used for building training dataset. """
+        R_count = len(self.topic_R_BERT_encodings)
+        N_count = len(self.topic_N_BERT_encodings)
+        print(R_count, N_count)
+        if (R_count == 0) or (N_count == 0):
+            self.topic_BERT_encodings = []
+            self.__process_sequential_topic()
+            return
+
+        def add_extra_sample(BERT_encodings, diff):
+            """ Even classes by sampling extra from . """
+            idx_list = list(range(len(BERT_encodings)))
+            for idx in random.choices(idx_list, k=diff):
+                BERT_encodings.append(BERT_encodings[idx])
+
+        # If less relevant documents sample extra relevant documents.
+        if R_count < N_count:
+            diff = abs(R_count - N_count)
+            add_extra_sample(BERT_encodings=self.topic_R_BERT_encodings, diff=diff)
+        # If less non-relevant documents sample extra non-relevant documents.
+        elif R_count > N_count:
+            diff = abs(R_count - N_count)
+            add_extra_sample(BERT_encodings=self.topic_N_BERT_encodings, diff=diff)
+
+        print(len(self.topic_R_BERT_encodings), len(self.topic_N_BERT_encodings))
+
+        # Add topics and shuffle data
+        self.topic_BERT_encodings = self.topic_R_BERT_encodings + self.topic_N_BERT_encodings
+        print(self.topic_BERT_encodings)
+        random.shuffle(self.topic_BERT_encodings)
+        print(self.topic_BERT_encodings)
+
+        # Process sequentially topics.
+        self.__process_sequential_topic()
 
 
-    def __process_topic(self, sequential, topic_BERT_encodings, topic_R_BERT_encodings, topic_N_BERT_encodings):
+    def __process_topic(self, sequential):
         """ Process topic - whether sequential (validation) or not sequential (training). """
         if sequential:
             # Sequential (validation dataset).
-            self.__process_sequential_topic(topic_BERT_encodings=topic_BERT_encodings)
+            self.__process_sequential_topic()
         else:
             # Not sequential (training dataset).
-            self.__process_non_sequential_topic(topic_R_BERT_encodings=topic_R_BERT_encodings,
-                                                topic_N_BERT_encodings=topic_N_BERT_encodings)
+            self.__process_non_sequential_topic()
+
+        # Update topic counter.
         self.topic_counter += 1
 
 
@@ -105,7 +158,7 @@ class TrecCarProcessing:
         """ Write data chuck to Pytorch TensorDataset and initialise new data chuck."""
         # Create data_dir_path if does not exist.
         if os.path.isdir(self.data_dir_path) == False:
-            print('Making directory: {}'.format())
+            print('Making directory: {}'.format(self.data_dir_path))
             os.mkdir(self.data_dir_path)
 
         print('Building chuck #{}'.format(self.chuck_counter))
@@ -141,12 +194,7 @@ class TrecCarProcessing:
         self.chuck_topic_size = chuck_topic_size
 
         with open(self.run_path) as f_run:
-            # Stores data for sequential dataset
-            topic_BERT_encodings = []
-            # Stores data for non-sequential dataset.
-            # Having data in relevant (R) and not relevant (N) allows to added extra R samples.
-            topic_R_BERT_encodings = []
-            topic_N_BERT_encodings = []
+
             # Store previous query so we know when a new topic began.
             topic_query = None
             for line in f_run:
@@ -157,13 +205,11 @@ class TrecCarProcessing:
                 if (topic_query != None) and (topic_query != query):
 
                     # Process topic
-                    self.__process_topic(sequential=sequential,
-                                         topic_BERT_encodings=topic_BERT_encodings,
-                                         topic_R_BERT_encodings=topic_R_BERT_encodings,
-                                         topic_N_BERT_encodings=topic_N_BERT_encodings)
+                    self.__process_topic(sequential=sequential)
+
                     # If specified data chuck size -> write chuck to file.
                     if self.topic_counter % self.chuck_topic_size == 0:
-                        print('WRITE DATA CHUCK')
+                        # write final chuck to file.
                         self.__write_chuck_to_directory()
 
                 # Decode query.
@@ -176,23 +222,21 @@ class TrecCarProcessing:
                                                             max_length=self.max_length,
                                                             add_special_tokens=True,
                                                             pad_to_max_length=True)
+                data = (query, doc_id, BERT_encodings)
                 # Append doc_id data topic
                 if sequential == True:
-                    topic_BERT_encodings.append((query, doc_id, BERT_encodings))
+                    self.topic_BERT_encodings.append(data)
                 else:
                     if doc_id in self.qrels[topic_query]:
-                        topic_R_BERT_encodings.append(BERT_encodings)
+                        self.topic_R_BERT_encodings.append(data)
                     else:
-                        topic_N_BERT_encodings.append(BERT_encodings)
+                        self.topic_N_BERT_encodings.append(data)
 
                 # Store query as topic query.
                 topic_query = query
 
         # Process any queries remaining.
-        self.__process_topic(sequential=sequential,
-                             topic_BERT_encodings=topic_BERT_encodings,
-                             topic_R_BERT_encodings=topic_R_BERT_encodings,
-                             topic_N_BERT_encodings=topic_N_BERT_encodings)
+        self.__process_topic(sequential=sequential)
 
         # write final chuck to file.
         self.__write_chuck_to_directory()
@@ -219,4 +263,5 @@ if __name__ == '__main__':
                                    data_dir_path=data_dir_path,
                                    tokenizer=tokenizer,
                                    max_length=max_length)
+
     processing.build_dataset(sequential=True)
