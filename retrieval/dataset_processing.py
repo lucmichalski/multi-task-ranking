c70d1c24533672666bc4c13751b3c140ff5e81de
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-13 15:27:00 +0100

    changing to smaller files

--- retrieval/dataset_processing.py
+++ retrieval/dataset_processing.py
@@ -169,8 +169,8 @@ if __name__ == '__main__':
     # data_dir_path = os.path.join(os.path.abspath(os.path.join(os.getcwd(), '..')), 'data', 'results')
 
     index_path = '/nfs/trec_car/index/anserini_paragraphs/lucene-index.car17v2.0.paragraphsv2'
-    run_path = '/nfs/trec_car/data/bert_reranker_datasets/test_100.run'
-    qrels_path = '/nfs/trec_car/data/bert_reranker_datasets/test_100.qrels'
+    run_path = '/nfs/trec_car/data/bert_reranker_datasets/dev_benchmark_Y1_25.run'
+    qrels_path = '/nfs/trec_car/data/bert_reranker_datasets/dev_benchmark_Y1_25.qrels'
     data_dir_path = '/nfs/trec_car/data/bert_reranker_datasets/'
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
