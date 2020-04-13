cf4f55a6ceaab801d559ca1a7b65eb7d7014b566
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-13 15:18:57 +0100

    moving test to main

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 
 from retrieval.tools import SearchTools, EvalTools, Pipeline
+from retrieval.dataset_processing import TrecCarProcessing
 from metadata import EntityPaths, PassagePaths
 import numpy as np
+from transformers import BertTokenizer
+
 
 if __name__ == '__main__':
 
 
-    results_dir = '/nfs/trec_car/data/entity_ranking/entity_parameter_tuning_4/'
+    # results_dir = '/nfs/trec_car/data/entity_ranking/entity_parameter_tuning_4/'
     #
     # run_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_passage_pyserini_10.run'
     # hits = 1000
@@ -29,15 +32,30 @@ if __name__ == '__main__':
     # eval = Eval()
     # eval.write_eval_from_qrels_and_run(run_path=run_path, qrels_path=passage_test_qrels_path, eval_config=eval_config)
     #
-    pipeline = Pipeline()
+    # pipeline = Pipeline()
+    #
+    # pipeline.search_BM25_tune_parameter(index_path=EntityPaths.entity_index,
+    #                                     topics_path=EntityPaths.entity_test_topics,
+    #                                     qrels_path=EntityPaths.entity_test_qrels,
+    #                                     results_dir=results_dir,
+    #                                     hits=1000,
+    #                                     b_list=[0.1, 0.15, 0.2, 0.25],
+    #                                     k1_list=[4.5, 5.5, 6.5, 7.5, 8.5])
 
-    pipeline.search_BM25_tune_parameter(index_path=EntityPaths.entity_index,
-                                        topics_path=EntityPaths.entity_test_topics,
-                                        qrels_path=EntityPaths.entity_test_qrels,
-                                        results_dir=results_dir,
-                                        hits=1000,
-                                        b_list=[0.1, 0.15, 0.2, 0.25],
-                                        k1_list=[4.5, 5.5, 6.5, 7.5, 8.5])
+    index_path = '/nfs/trec_car/index/anserini_paragraphs/lucene-index.car17v2.0.paragraphsv2'
+    run_path = '/nfs/trec_car/data/bert_reranker_datasets/test_100.run'
+    qrels_path = '/nfs/trec_car/data/bert_reranker_datasets/test_100.qrels'
+    data_dir_path = '/nfs/trec_car/data/bert_reranker_datasets/'
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    max_length = 512
+    processing = TrecCarProcessing(qrels_path=qrels_path,
+                                   run_path=run_path,
+                                   index_path=index_path,
+                                   data_dir_path=data_dir_path,
+                                   tokenizer=tokenizer,
+                                   max_length=max_length)
+    processing.build_dataset()
 
 
 
