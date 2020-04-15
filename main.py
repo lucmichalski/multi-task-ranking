e7c6928748ccc0a8717ecc67fffbe125825d4350
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-15 17:59:53 +0100

    think fixed inference bug

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
     # eval = EvalTools()
     # eval.write_eval_from_qrels_and_run(run_path=run_path, qrels_path=qrels_path, eval_config=eval_config)
 
-    train_data_dir_path = '/nfs/trec_car/data/bert_reranker_datasets/train_benchmarkY1_chunks/'
-    train_batch_size = 8
+    train_data_dir_path = None #'/nfs/trec_car/data/bert_reranker_datasets/train_benchmarkY1_chunks/'
+    train_batch_size = None #8
     dev_data_dir_path = '/nfs/trec_car/data/bert_reranker_datasets/dev_benchmark_Y1_25_chucks/'
     dev_batch_size = 64
     dev_qrels_path = '/nfs/trec_car/data/bert_reranker_datasets/dev_benchmark_Y1_25.qrels'
@@ -35,26 +35,26 @@ if __name__ == '__main__':
                                                 dev_qrels_path=dev_qrels_path,
                                                 dev_run_path=dev_run_path)
 
-    epochs = 5
-    lr = 2e-5
-    eps = 1e-8
-    weight_decay = 0.01
-    num_warmup_steps = 0
-    experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'testing_training_and_validation_less_printing_v2'
-    write = True
-    logging_steps = 600
-
-    experiment.run_experiment_single_head(
-                                    head_flag='entity',
-                                    epochs=epochs,
-                                    lr=lr,
-                                    eps=eps,
-                                    weight_decay=weight_decay,
-                                    num_warmup_steps=num_warmup_steps,
-                                    experiments_dir=experiments_dir,
-                                    experiment_name=experiment_name,
-                                    logging_steps=logging_steps)
+    # epochs = 5
+    # lr = 2e-5
+    # eps = 1e-8
+    # weight_decay = 0.01
+    # num_warmup_steps = 0
+    # experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
+    # experiment_name = 'testing_training_and_validation_less_printing_v2'
+    # write = True
+    # logging_steps = 600
+    #
+    # experiment.run_experiment_single_head(
+    #                                 head_flag='entity',
+    #                                 epochs=epochs,
+    #                                 lr=lr,
+    #                                 eps=eps,
+    #                                 weight_decay=weight_decay,
+    #                                 num_warmup_steps=num_warmup_steps,
+    #                                 experiments_dir=experiments_dir,
+    #                                 experiment_name=experiment_name,
+    #                                 logging_steps=logging_steps)
     head_flag = 'entity'
     rerank_run_path = '/nfs/trec_car/data/bert_reranker_datasets/test_runs/initial_new_pipeline.run'
     experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path)
