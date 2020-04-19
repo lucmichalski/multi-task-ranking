bea6bd6cf88d966a9625fe746b67291b47002aa0
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-19 17:41:03 +0100

    test entites

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     train_batch_size = None #8
     dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_entity_para_1000_chunks/'
     dev_batch_size = 64 * 4
-    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_entity.qrels'
+    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/test_entity.qrels'
     dev_run_path = '/nfs/trec_car/data/entity_ranking/test_hierarchical_entity_1000.run'
     model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_entity_100_lr_8e6_num_warmup_steps_2000_just_para/epoch1_batch14000/'
     experiment = FineTuningReRankingExperiments(model_path=model_path,
