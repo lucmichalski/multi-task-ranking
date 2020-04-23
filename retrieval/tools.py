c4f8965ef4a427eb2ad67dc25cb3e0acd8d82436
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-23 13:14:58 +0100

    added orancle re-ranking

--- retrieval/tools.py
+++ retrieval/tools.py
@@ -391,6 +391,7 @@ class EvalTools:
                         run_oracle = sorted(run, reverse=True)
                         query_metrics_oracle, _ = self.get_query_metrics(run=run_oracle, R=R, eval_config=eval_config)
                         query_metrics_oracle_sum = dict(Counter(query_metrics_oracle_sum)+Counter(query_metrics_oracle))
+                        print(query_metrics_oracle_sum)
                         # Start next query.
                         run_doc_ids = []
                         run = []
