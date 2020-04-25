fff45f4216f5ea5a5da2d487fdb18290dbf4c200
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-25 09:25:07 +0100

    running on manual Y2 qrels

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
     # data_dir_paths = ['/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_passage_train_data/benchmarkY1_tree_passage_train_100_chunks/','/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_passage_dev_data/benchmarkY1_tree_passage_dev_100_chunks/', '/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_no_root_passage_train_data/benchmarkY1_tree_no_root_passage_train_100_chunks/','/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_no_root_passage_dev_data/benchmarkY1_tree_no_root_passage_dev_100_chunks/']
     # training_datasets = [True, False, True, False]
 
-    run_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000.run', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000.run', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage_1000.run']
-    qrels_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.qrels', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.qrels', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage.qrels']
-    topics_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.topics', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.topics', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage.topics']
-    data_dir_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000_chunks', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000_chunks', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage_1000_chunks']
-    training_datasets = [False, False, False]
-    index_paths = [EntityPaths.index, EntityPaths.index, PassagePaths.index]
+    # run_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000.run', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000.run', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage_1000.run']
+    # qrels_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.qrels', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.qrels', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage.qrels']
+    # topics_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.topics', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.topics', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage.topics']
+    # data_dir_paths = ['/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000_chunks', '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000_chunks', '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage_1000_chunks']
+    # training_datasets = [False, False, False]
+    # index_paths = [EntityPaths.index, EntityPaths.index, PassagePaths.index]
 
     # hits = 100
     # printing_step = 100
@@ -35,35 +35,35 @@ if __name__ == '__main__':
     #     'ndcg': {'k': 20},
     # }
     #
-    for run_path, qrels_path, topics_path, data_dir_path, training_dataset, index_path in zip(run_paths, qrels_paths, topics_paths, data_dir_paths, training_datasets, index_paths):
+    # for run_path, qrels_path, topics_path, data_dir_path, training_dataset, index_path in zip(run_paths, qrels_paths, topics_paths, data_dir_paths, training_datasets, index_paths):
         # print('searching')
         # search = SearchTools(index_path=index_path, searcher_config=searcher_config)
         # search.write_run_from_topics(topics_path=topics_path, run_path=run_path, hits=hits, printing_step=printing_step)
         # print('eval')
         # eval = EvalTools()
         # eval.write_eval_from_qrels_and_run(run_path=run_path, qrels_path=qrels_path, eval_config=eval_config)
-        print('dataset')
-        processing = TrecCarProcessing(qrels_path=qrels_path,
-                                       run_path=run_path,
-                                       index_path=index_path,
-                                       data_dir_path=data_dir_path)
-
-        processing.build_dataset(training_dataset=training_dataset, chuck_topic_size=50, first_para=True)
+        # print('dataset')
+        # processing = TrecCarProcessing(qrels_path=qrels_path,
+        #                                run_path=run_path,
+        #                                index_path=index_path,
+        #                                data_dir_path=data_dir_path)
+        #
+        # processing.build_dataset(training_dataset=training_dataset, chuck_topic_size=50, first_para=True)
     #
-    # train_data_dir_path = None #'/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_no_root_passage_train_data/benchmarkY1_tree_no_root_passage_train_100_chunks/'
-    # train_batch_size = None #8
-    # dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY1_tree_no_root_passage_data/testY1_tree_no_root_passage_1000_chunks/'
-    # dev_batch_size = 64 * 8
-    # dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY1_tree_no_root_passage_data/testY1_tree_no_root_passage.qrels'
-    # dev_run_path = '/nfs/trec_car/data/entity_ranking/testY1_tree_no_root_passage_data/testY1_tree_no_root_passage_1000.run'
-    # model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_tree_no_root_passage_100_lr_8e6_num_warmup_steps_0.1/epoch1_batch14000/'
-    # experiment = FineTuningReRankingExperiments(model_path=model_path,
-    #                                             train_data_dir_path=train_data_dir_path,
-    #                                             train_batch_size=train_batch_size,
-    #                                             dev_data_dir_path=dev_data_dir_path,
-    #                                             dev_batch_size=dev_batch_size,
-    #                                             dev_qrels_path=dev_qrels_path,
-    #                                             dev_run_path=dev_run_path)
+    train_data_dir_path = None #'/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_no_root_passage_train_data/benchmarkY1_tree_no_root_passage_train_100_chunks/'
+    train_batch_size = None #8
+    dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage_1000_chunks/'
+    dev_batch_size = 64 * 8
+    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage.qrels'
+    dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_passage_data/testY2_manual_passage_100.run'
+    model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_passage_100_lr_8e6_num_warmup_steps_1000/'
+    experiment = FineTuningReRankingExperiments(model_path=model_path,
+                                                train_data_dir_path=train_data_dir_path,
+                                                train_batch_size=train_batch_size,
+                                                dev_data_dir_path=dev_data_dir_path,
+                                                dev_batch_size=dev_batch_size,
+                                                dev_qrels_path=dev_qrels_path,
+                                                dev_run_path=dev_run_path)
 
     # epochs = 2
     # lr = 8e-6
@@ -87,6 +87,6 @@ if __name__ == '__main__':
     #                                 experiment_name=experiment_name,
     #                                 logging_steps=logging_steps)
 
-    # head_flag = 'passage'
-    # rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/benchmarkY1_tree_no_root_passage_100_lr_8e6_num_warmup_steps_0.1_test_model_1-14000.run'
-    # experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path)
+    head_flag = 'passage'
+    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/benchmarkY1_hierarchical_passage_100_model_test_Y2_manual.run'
+    experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path)
