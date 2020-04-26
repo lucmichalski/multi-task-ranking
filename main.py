f950357e72b02448b234896a8c6f87ab5c2869ad
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-04-26 21:33:22 +0100

    infernece on automatic entity

--- main.py
+++ main.py
@@ -52,11 +52,11 @@ if __name__ == '__main__':
     #
     train_data_dir_path = None #'/nfs/trec_car/data/entity_ranking/benchmarkY1_tree_no_root_passage_train_data/benchmarkY1_tree_no_root_passage_train_100_chunks/'
     train_batch_size = None #8
-    dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000_chunks/'
+    dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000_chunks/'
     dev_batch_size = 64 * 8
-    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.qrels'
-    dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000.run'
-    model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_article_entity_para_500_lr_1e5_num_warmup_steps_100/epoch1_batch1300/'
+    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.qrels'
+    dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000.run'
+    model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_toplevel_entity_para_250_lr_1e5_num_warmup_steps_100/epoch1_batch2600/'
     experiment = FineTuningReRankingExperiments(model_path=model_path,
                                                 train_data_dir_path=train_data_dir_path,
                                                 train_batch_size=train_batch_size,
@@ -88,5 +88,5 @@ if __name__ == '__main__':
     #                                 logging_steps=logging_steps)
 
     head_flag = 'entity'
-    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/benchmarkY1_article_entity_500_model_test_Y2_manual_entity.run'
+    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/benchmarkY1_toplevel_entity_250_model_test_Y2_automatic_entity.run'
     experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path)
