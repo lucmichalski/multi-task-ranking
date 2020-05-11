abdafdba9b5163a00c4a87a6f5e6073a6bb89739
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-05-11 23:23:04 +0100

    ready for 1000 inference with lower lr

--- main.py
+++ main.py
@@ -49,12 +49,12 @@ if __name__ == '__main__':
                                                 dev_run_path=dev_run_path)
 
     epochs = 2
-    lr = 2e-6
+    lr = 8e-7
     eps = 1e-8
     weight_decay = 0.01
     warmup_percentage = 0.1
     experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'benchmarkY1_hierarchical_entity_1000_lr_2e6_num_warmup_steps_0.1_new_pipeline'
+    experiment_name = 'benchmarkY1_hierarchical_entity_1000_lr_8e7_num_warmup_steps_0.1_new_pipeline'
     write = True
     logging_steps = 2000
     head_flag = 'entity'
