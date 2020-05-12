0cf63b30838ee8e71379dea66d53a15abed51b90
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-05-12 22:49:37 +0100

    inference of 300 on Y2 test automatic

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ if __name__ == '__main__':
     dev_batch_size = 64 * 4
     dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.qrels'
     dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000.run'
-    model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_hierarchical_entity_1000_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch44000/'
+    #model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_hierarchical_entity_1000_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch44000/'
+    model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_hierarchical_entity_300_lr_5e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch11000/'
     experiment = FineTuningReRankingExperiments(model_path=model_path,
                                                 train_data_dir_path=train_data_dir_path,
                                                 train_batch_size=train_batch_size,
@@ -71,5 +72,5 @@ if __name__ == '__main__':
     #                                 logging_steps=logging_steps)
 
     head_flag = 'entity'
-    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/benchmarkY1_hierarchical_entity_1000_test_Y2_automatic_entity.run'
+    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/benchmarkY1_hierarchical_entity_300_test_Y2_automatic_entity.run'
     experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path)
