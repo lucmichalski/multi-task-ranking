f2c9b84d796ce86187c17c87d074ea4e75e2326e
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-05-24 17:02:14 +0100

    inference 10000 manual

--- main.py
+++ main.py
@@ -43,10 +43,13 @@ if __name__ == '__main__':
 
     train_data_dir_path = None #'/nfs/trec_car/data/entity_ranking/full_data/full_data_v2_hierarchical_10000_hits_300_chunks'
     train_batch_size = None #10
-    dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000_chunks/'
+    dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000_chunks/'
+    #dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000_chunks/'
     dev_batch_size = 64 * 8
-    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.qrels'
-    dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000.run'
+    dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.qrels'
+    dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_1000.run'
+    #dev_qrels_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.qrels'
+    #dev_run_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_1000.run'
     model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/full_data_v2_hierarchical_10000_hits_300_v2_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch225000'
     experiment = FineTuningReRankingExperiments(model_path=model_path,
                                                 train_data_dir_path=train_data_dir_path,
@@ -80,5 +83,5 @@ if __name__ == '__main__':
     #                                 logging_steps=logging_steps)
 
     head_flag = 'entity'
-    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/full_docs_10000_hits_300_lr_2e6_hierarchical_entity_300_test_Y2_automatic_entity.run'
+    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/full_docs_10000_hits_300_lr_2e6_hierarchical_entity_300_test_Y2_manual_entity.run'
     experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path)
