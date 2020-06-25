68d925c8eb35e89abd027d096f4607a07b189a46
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-06-25 11:54:51 +0100

    train and dev with new roberta pipeline v3 - test inference a

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ if __name__ == '__main__':
     # processing.build_dataset(training_dataset=training_dataset, chuck_topic_size=50, first_para=False)
 
 
-    train_data_dir_path = None #'/nfs/trec_car/data/passage_ranking/dtrain_benchmarkY1_250_roberta_chunks/'
-    train_batch_size = None #12
+    train_data_dir_path = '/nfs/trec_car/data/passage_ranking/dtrain_benchmarkY1_250_roberta_chunks/'
+    train_batch_size = 12
     dev_batch_size = 64 * 8
     dev_data_dir_path = '/nfs/trec_car/data/passage_ranking/dev_benchmark_Y1_25_roberta_chunks/'
     dev_qrels_path = '/nfs/trec_car/data/passage_ranking/dev_benchmark_Y1_25.qrels'
@@ -64,29 +64,29 @@ if __name__ == '__main__':
                                                 dev_qrels_path=dev_qrels_path,
                                                 dev_run_path=dev_run_path)
 
-    # epochs = 2
-    # lr = 6e-6
-    # eps = 1e-8
-    # weight_decay = 0.01
-    # warmup_percentage = 0.1
-    # experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    # experiment_name = 'roberta_benchmarkY1_lr_6e6_v2'
-    # write = True
-    # logging_steps = 1000
-    # head_flag = 'passage'
-    #
-    # experiment.run_experiment_single_head(
-    #     head_flag=head_flag,
-    #     epochs=epochs,
-    #     lr=lr,
-    #     eps=eps,
-    #     weight_decay=weight_decay,
-    #     warmup_percentage=warmup_percentage,
-    #     experiments_dir=experiments_dir,
-    #     experiment_name=experiment_name,
-    #     logging_steps=logging_steps
-    # )
-
+    epochs = 2
+    lr = 2e-5
+    eps = 1e-8
+    weight_decay = 0.01
+    warmup_percentage = 0.1
+    experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
+    experiment_name = 'roberta_benchmarkY1_lr_2e5_v3'
+    write = True
+    logging_steps = 500
     head_flag = 'passage'
-    rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/roberta_dev_test_write.run'
-    experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path, do_eval=True)
+
+    experiment.run_experiment_single_head(
+        head_flag=head_flag,
+        epochs=epochs,
+        lr=lr,
+        eps=eps,
+        weight_decay=weight_decay,
+        warmup_percentage=warmup_percentage,
+        experiments_dir=experiments_dir,
+        experiment_name=experiment_name,
+        logging_steps=logging_steps
+    )
+
+    # head_flag = 'passage'
+    # rerank_run_path = '/nfs/trec_car/data/entity_ranking/test_runs/roberta_dev_test_write.run'
+    # experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path, do_eval=True)
