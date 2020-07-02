383e7c11819a1587382d16e0d8c86e0ad19ff0b7
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-02 22:33:40 +0100

    added data parralle wrapper to test if works

--- main.py
+++ main.py
@@ -52,10 +52,10 @@ if __name__ == '__main__':
     #
     #         processing.build_dataset(training_dataset=training_dataset, chuck_topic_size=100, first_para=False)
 
-
+    gpus = 4
     train_data_dir_path = '/nfs/trec_car/data/entity_ranking/benchmarkY1_hierarchical_passage_train_data/benchmarkY1_hierarchical_passage_train_100_chunks/'
-    train_batch_size = 8
-    dev_batch_size = 64 * 4
+    train_batch_size = 8 * gpus
+    dev_batch_size = 64 * 4 * gpus
     dev_data_dir_path = '/nfs/trec_car/data/entity_ranking/benchmarkY1_hierarchical_passage_dev_data/benchmarkY1_hierarchical_passage_dev_100_chunks/'
     dev_qrels_path = '/nfs/trec_car/data/entity_ranking/benchmarkY1_hierarchical_passage_dev_data/benchmarkY1_dev_passage.qrels'
     dev_run_path = '/nfs/trec_car/data/entity_ranking/benchmarkY1_hierarchical_passage_dev_data/benchmarkY1_dev_passage_100.run'
