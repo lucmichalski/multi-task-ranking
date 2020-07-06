4a5994b90510c943bdbcd228fce0d0f31cdc9606
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-06 16:50:00 +0100

    experiment with context on train/dev

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     weight_decay = 0.01
     warmup_percentage = 0.1
     experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'bert_with_context_no_ent_v1_8e6'
+    experiment_name = 'bert_with_context_v2_1e5'
     write = True
     logging_steps = 500
     head_flag = 'entity'
