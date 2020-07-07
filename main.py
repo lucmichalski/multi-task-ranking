bff570b2959c4af95df2ae4803c5550e99c08788
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-07 11:32:03 +0100

    ready to train with lower lr entity + context

--- main.py
+++ main.py
@@ -70,14 +70,14 @@ if __name__ == '__main__':
                                                 dev_run_path=dev_run_path)
 
     epochs = 2
-    lr = 5e-6
+    lr = 2e-6
     eps = 1e-8
     weight_decay = 0.01
     warmup_percentage = 0.1
     experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'bert_with_context_v2_no_sep_5e6_top5_ents'
+    experiment_name = 'bert_with_context_v2_no_sep_2e6_top5_ents'
     write = True
-    logging_steps = 500
+    logging_steps = 1000
     head_flag = 'entity'
 
     experiment.run_experiment_single_head(
