8950e7ce4f28216f092eedae23e7de0d09a2cbed
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-09 09:34:40 +0100

    testing multi task 1e6

--- main.py
+++ main.py
@@ -110,12 +110,12 @@ if __name__ == '__main__':
                                                 dev_run_path_entity=dev_run_path_entity)
 
     epochs = 3
-    lr = 3e-6
+    lr = 1e-6
     eps = 1e-8
     weight_decay = 0.01
     warmup_percentage = 0.1
     experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'test_multi_task_hierarchical_v5_3e6'
+    experiment_name = 'test_multi_task_hierarchical_v5_1e6'
     write = True
     logging_steps = 500
 
