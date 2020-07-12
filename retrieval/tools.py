70bccdf0bf005bed396f52a3d6ffc210739e248e
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-12 10:24:58 +0100

    added functionality to process contents

--- retrieval/tools.py
+++ retrieval/tools.py
@@ -350,7 +350,7 @@ class SearchTools:
                             query = text
                         else:
                             query += " " + text
-            return query
+            return query[:100]
 
 
     def write_entity_run_news(self, run_path, qrels_path, query_type, hits=250000, news_index_path=NewsPassagePaths.index):
@@ -383,7 +383,6 @@ class SearchTools:
                     missed_hits = list(set(valid_docs) - set([hit[0] for hit in valid_hits]))
                     min_score = valid_hits[len(valid_hits)-1][1]
                 for doc_id in missed_hits:
-                    print('missed: {}'.format(doc_id))
                     # Create and write run file.
                     min_score -= 0.1
                     run_line = " ".join((query_id, "Q0", doc_id, str(rank), "{:.6f}".format(min_score), "PYSERINI")) + '\n'
