a76c7b7359ec973358cf8dfc0ab627a7fdec8d4c
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-13 20:01:36 +0100

    ready to build dev entity dataset

--- main.py
+++ main.py
@@ -9,52 +9,53 @@ from torch import nn
 
 if __name__ == '__main__':
 
-    from metadata import NewsPassagePaths, CarEntityPaths
-    from retrieval.tools import SearchTools
-
-    run_paths = [#'/nfs/trec_news_track/runs/anserini/bert/news_track.dev.bm25.500000.title+contents.100_words.run',
-                 '/nfs/trec_news_track/runs/anserini/bert/news_track.train.bm25.500000.title+contents.100_words.run',
-                 '/nfs/trec_news_track/runs/anserini/bert/news_track.test.bm25.500000.title+contents.100_words.run']
-    qrels_paths = [#'/nfs/trec_news_track/bert/dev_entity/news_track.dev.entity.qrels',
-                   '/nfs/trec_news_track/bert/train_entity/news_track.train.entity.qrels',
-                   '/nfs/trec_news_track/bert/test_entity/news_track.test.entity.qrels']
-
-    query_type = 'title+contents'
-    hits = 500000
-    news_index_path = NewsPassagePaths.index
-    words = 100
-
-    for run_path, qrels_path in zip(run_paths, qrels_paths):
-        search_tools = SearchTools(index_path=CarEntityPaths.index)
-        search_tools.write_entity_run_news(run_path, qrels_path, query_type, words, hits, news_index_path)
-
-    # from retrieval.dataset_processing import DatasetProcessing
-    # from metadata import NewsPassagePaths
+    # from metadata import NewsPassagePaths, CarEntityPaths
+    # from retrieval.tools import SearchTools
     #
-    # qrels_path = '/nfs/trec_news_track/data/2019/newsir19-qrels-background.txt'
-    # xml_topics_path = '/nfs/trec_news_track/data/2019/newsir19-background-linking-topics.xml'
-    # run_path = '/nfs/trec_news_track/bert/test_passage/news_track.test.passage.250.bm25.rm3.run'
-    # index_path = NewsPassagePaths.index
-    # data_dir_path = '/nfs/trec_news_track/bert/test_passage/news_track_test_passage_250_bm25_rm3_bert_chunks/'
-    # max_length = 512
-    # context_path = None
-    # training_dataset = False
-    # ranking_type = 'entity'
+    # run_paths = [#'/nfs/trec_news_track/runs/anserini/bert/news_track.dev.bm25.500000.title+contents.100_words.run',
+    #              '/nfs/trec_news_track/runs/anserini/bert/news_track.train.bm25.500000.title+contents.100_words.run',
+    #              '/nfs/trec_news_track/runs/anserini/bert/news_track.test.bm25.500000.title+contents.100_words.run']
+    # qrels_paths = [#'/nfs/trec_news_track/bert/dev_entity/news_track.dev.entity.qrels',
+    #                '/nfs/trec_news_track/bert/train_entity/news_track.train.entity.qrels',
+    #                '/nfs/trec_news_track/bert/test_entity/news_track.test.entity.qrels']
+    #
     # query_type = 'title+contents'
+    # hits = 500000
+    # news_index_path = NewsPassagePaths.index
+    # words = 100
     #
-    # processing = DatasetProcessing(qrels_path=qrels_path,
-    #                                run_path=run_path,
-    #                                index_path=index_path,
-    #                                data_dir_path=data_dir_path,
-    #                                max_length=max_length,
-    #                                context_path=context_path)
-    #
-    # processing.build_news_dataset(training_dataset=training_dataset,
-    #                               chuck_topic_size=1e8,
-    #                               ranking_type=ranking_type,
-    #                               query_type=query_type,
-    #                               car_index_path=None,
-    #                               xml_topics_path=xml_topics_path)
+    # for run_path, qrels_path in zip(run_paths, qrels_paths):
+    #     search_tools = SearchTools(index_path=CarEntityPaths.index)
+    #     search_tools.write_entity_run_news(run_path, qrels_path, query_type, words, hits, news_index_path)
+
+    from retrieval.dataset_processing import DatasetProcessing
+    from metadata import NewsPassagePaths
+
+    qrels_path = '/nfs/trec_news_track/bert/dev_entity/news_track.dev.entity.qrels'
+    xml_topics_path = None #'/nfs/trec_news_track/data/2019/newsir19-background-linking-topics.xml'
+    run_path = '/nfs/trec_news_track/runs/anserini/bert/news_track.dev.bm25.500000.title+contents.100_words.run'
+    index_path = NewsPassagePaths.index
+    car_index_path = CarEntityPaths.index
+    data_dir_path = '/nfs/trec_news_track/runs/anserini/bert/news_track_dev_bm25_500000_100_words_bert_chunks/'
+    max_length = 512
+    context_path = None
+    training_dataset = False
+    ranking_type = 'entity'
+    query_type = 'title+contents'
+
+    processing = DatasetProcessing(qrels_path=qrels_path,
+                                   run_path=run_path,
+                                   index_path=index_path,
+                                   data_dir_path=data_dir_path,
+                                   max_length=max_length,
+                                   context_path=context_path)
+
+    processing.build_news_dataset(training_dataset=training_dataset,
+                                  chuck_topic_size=1e8,
+                                  ranking_type=ranking_type,
+                                  query_type=query_type,
+                                  car_index_path=car_index_path,
+                                  xml_topics_path=xml_topics_path)
 
     # gpus = 1
     # model_path =  '/nfs/trec_car/data/bert_reranker_datasets/exp/test_trec_news_v2_2e5_batch_8/epoch1_batch1000/'
@@ -103,8 +104,8 @@ if __name__ == '__main__':
     #     logging_steps=logging_steps
     # )
 
-    head_flag = 'passage'
-    rerank_run_path = '/nfs/trec_news_track/runs/bert/background_2019/re_ranked_anserini.bm5.rm3.run'
-    experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path, do_eval=False, cap_rank=100)
+    # head_flag = 'passage'
+    # rerank_run_path = '/nfs/trec_news_track/runs/bert/background_2019/re_ranked_anserini.bm5.rm3.run'
+    # experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path, do_eval=False, cap_rank=100)
 
 
