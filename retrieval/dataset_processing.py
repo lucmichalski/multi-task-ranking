1fc8b5a0c240dd2bae83cbc3cbeed6b968b7efbd
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-13 10:37:35 +0100

    ready to test building bert datatset

--- retrieval/dataset_processing.py
+++ retrieval/dataset_processing.py
@@ -305,6 +305,8 @@ class DatasetProcessing:
                     query_id = passage_id_map[query_id]
                     query_dict = json.loads(self.search_tools.get_contents_from_docid(doc_id=query_id))
                     query = self.search_tools.process_news_query(query_dict=query_dict, query_type=query_type)
+                    print('<------------->')
+                    print("query: {}".format(query))
                 else:
                     query_dict = self.search_tools.get_contents_from_docid(doc_id=query_id)
                     query = self.search_tools.process_news_query(query_dict=query_dict, query_type=query_type)
@@ -312,8 +314,10 @@ class DatasetProcessing:
                 # Extract text from index using doc_id.
                 if self.context_path == None:
                     if ranking_type == 'passage':
-                        text_dict = json.loads(self.search_tools.get_contents_from_docid(doc_id=doc_id)
+                        text_dict = json.loads(self.search_tools.get_contents_from_docid(doc_id=doc_id))
                         text = self.search_tools.process_news_query(query_dict=text_dict, query_type=query_type)
+                        print("text: {}".format(query))
+
                     else:
                         text = search_tools_car.get_contents_from_docid(doc_id=doc_id)
 
