abe58ca2989bd6a90e2487f1ab7380d95ac11a1e
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-16 11:40:49 +0100

    ready to build train dataset with scaled relevance

--- main.py
+++ main.py
@@ -45,15 +45,15 @@ if __name__ == '__main__':
     # from retrieval.dataset_processing import DatasetProcessing
     # from metadata import NewsPassagePaths
     #
-    qrels_path = '/nfs/trec_news_track/bert/test_entity/news_track.test.entity.qrels'
+    qrels_path = '/nfs/trec_news_track/bert/train_entity/news_track.train.entity.qrels'
     xml_topics_path = None #'/nfs/trec_news_track/data/2019/newsir19-background-linking-topics.xml'
-    run_path = '/nfs/trec_news_track/runs/anserini/bert/news_track.test.bm25.100000.title+contents.50_words.run'
+    run_path = '/nfs/trec_news_track/runs/anserini/bert/news_track.train.bm25.100000.title+contents.50_words.run'
     index_path = NewsPassagePaths.index
     car_index_path = CarEntityPaths.index
-    data_dir_path = '/nfs/trec_news_track/runs/anserini/bert/news_track_test_bm25_100000_50_words_bert_chunks_scaled_rel/'
+    data_dir_path = '/nfs/trec_news_track/runs/anserini/bert/news_track_train_bm25_100000_50_words_bert_chunks_scaled_rel/'
     max_length = 512
     context_path = None
-    training_dataset = False
+    training_dataset = True
     ranking_type = 'entity'
     query_type = 'title+contents'
 
