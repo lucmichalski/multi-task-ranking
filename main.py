77fa7c642110825a7ede30ce4f7084317bf9ce34
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-07-16 12:34:10 +0100

    inference on test

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     gpus = 1
     model_path = None #'/nfs/trec_car/data/bert_reranker_datasets/exp/test_trec_news_v2_entity_2e5_batch_8_fixed_qrels/epoch3_batch75'
     extra_layers = False
-    train_batch_size = 8 * gpus
+    train_batch_size = None #8 * gpus
     dev_batch_size = 64 * 3 * gpus
 
     # train_data_dir_path_passage = None #'/nfs/trec_news_track/runs/anserini/bert/news_track_train_bm25_100000_50_words_bert_chunks/'
@@ -82,10 +82,10 @@ if __name__ == '__main__':
     # dev_qrels_path_passage = '/nfs/trec_news_track/bert/dev_entity/news_track.dev.entity.qrels'
     # dev_run_path_passage = '/nfs/trec_news_track/runs/anserini/bert/news_track.dev.bm25.100000.title+contents.50_words.run'
 
-    train_data_dir_path_entity = '/nfs/trec_news_track/runs/anserini/bert/news_track_train_bm25_100000_50_words_bert_chunks_scaled_rel/'
-    dev_data_dir_path_entity =  '/nfs/trec_news_track/runs/anserini/bert/news_track_dev_bm25_100000_50_words_bert_chunks_scaled_rel/'
-    dev_qrels_path_entity = '/nfs/trec_news_track/bert/dev_entity/news_track.dev.entity.qrels'
-    dev_run_path_entity = '/nfs/trec_news_track/runs/anserini/bert/news_track.dev.bm25.100000.title+contents.50_words.run'
+    train_data_dir_path_entity = None #'/nfs/trec_news_track/runs/anserini/bert/news_track_train_bm25_100000_50_words_bert_chunks_scaled_rel/'
+    dev_data_dir_path_entity = '/nfs/trec_news_track/runs/anserini/bert/news_track_test_bm25_100000_50_words_bert_chunks_scaled_rel/'
+    dev_qrels_path_entity = '/nfs/trec_news_track/bert/test_entity/news_track.test.entity.qrels'
+    dev_run_path_entity = '/nfs/trec_news_track/runs/anserini/bert/news_track.test.bm25.100000.title+contents.50_words.run'
 
     experiment = FineTuningReRankingExperiments(model_path=model_path,
                                                 extra_layers=extra_layers,
@@ -97,32 +97,32 @@ if __name__ == '__main__':
                                                 dev_qrels_path_entity=dev_qrels_path_entity,
                                                 dev_run_path_entity=dev_run_path_entity)
 
-    epochs = 3
-    lr = 4e-5
-    eps = 1e-8
-    weight_decay = 0.01
-    warmup_percentage = 0.1
-    experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
-    experiment_name = 'test_trec_news_v2_entity_4e5_batch_8_fixed_qrels_scaled_rel'
-    write = True
-    logging_steps = 25
-    head_flag = 'entity'
-
-    experiment.run_experiment_single_head(
-        head_flag=head_flag,
-        epochs=epochs,
-        lr=lr,
-        eps=eps,
-        weight_decay=weight_decay,
-        warmup_percentage=warmup_percentage,
-        experiments_dir=experiments_dir,
-        experiment_name=experiment_name,
-        logging_steps=logging_steps
-    )
-
+    # epochs = 3
+    # lr = 4e-5
+    # eps = 1e-8
+    # weight_decay = 0.01
+    # warmup_percentage = 0.1
+    # experiments_dir = '/nfs/trec_car/data/bert_reranker_datasets/exp/'
+    # experiment_name = 'test_trec_news_v2_entity_4e5_batch_8_fixed_qrels_scaled_rel'
+    # write = True
+    # logging_steps = 25
     # head_flag = 'entity'
-    # rerank_run_path = '/nfs/trec_news_track/runs/bert/entity_2019/test_entity_news_fixed_qrels.bm5_re_rank.run'
-    # experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path, do_eval=False, cap_rank=100)
+    #
+    # experiment.run_experiment_single_head(
+    #     head_flag=head_flag,
+    #     epochs=epochs,
+    #     lr=lr,
+    #     eps=eps,
+    #     weight_decay=weight_decay,
+    #     warmup_percentage=warmup_percentage,
+    #     experiments_dir=experiments_dir,
+    #     experiment_name=experiment_name,
+    #     logging_steps=logging_steps
+    # )
+
+    head_flag = 'entity'
+    rerank_run_path = '/nfs/trec_news_track/runs/bert/entity_2018/test_entity_news_fixed_qrels_scaled_rel.bm5_re_rank.run'
+    experiment.inference(head_flag=head_flag, rerank_run_path=rerank_run_path, do_eval=False, cap_rank=100)
     #
     # from REL.mention_detection import MentionDetection
     # from REL.utils import process_results
