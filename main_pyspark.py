029f60f56716044003a9e9b87be811f0a577d341
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-08-05 18:14:14 +0100

    testing pyspark

--- main_pyspark.py
+++ main_pyspark.py
@@ -30,24 +30,24 @@ if __name__ == '__main__':
     from pyspark_processing.trec_news_pipeline import write_article_data_to_dir, run_pyspark_pipeline
 
     read_path = '/nfs/trec_news_track/WashingtonPost.v2/data/TREC_Washington_Post_collection.v2.jl'
-    dir_path = '/nfs/trec_news_track/index/test_10000_chunks/'
-    num_docs = 100
-    chunks = 10
-    print_intervals = 10
+    dir_path = '/nfs/trec_news_track/index/test_500_chunks/'
+    num_docs = 500
+    chunks = 100
+    print_intervals = 100
     write_output = True
     rel_wiki_year = '2019'
     rel_base_url = '/nfs/trec_car/entity_processing/REL/'
     rel_model_path = rel_base_url + 'ed-wiki-{}/model'.format(rel_wiki_year)
     car_id_to_name_path = '/nfs/trec_news_track/lmdb.map_id_to_name.v1'
 
-    # write_article_data_to_dir(read_path=read_path,
-    #                           dir_path=dir_path,
-    #                           num_docs=num_docs,
-    #                           chunks=chunks,
-    #                           print_intervals=print_intervals,
-    #                           write_output=write_output)
+    write_article_data_to_dir(read_path=read_path,
+                              dir_path=dir_path,
+                              num_docs=num_docs,
+                              chunks=chunks,
+                              print_intervals=print_intervals,
+                              write_output=write_output)
 
-    out_path = '/nfs/trec_news_track/index/test_10000_out_v4/'
+    out_path = '/nfs/trec_news_track/index/test_500_out_v1/'
     run_pyspark_pipeline(dir_path,
                          spark,
                          cores,
