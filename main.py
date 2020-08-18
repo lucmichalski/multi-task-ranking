24e701a99a97eaed3631d17f366be68a734182bf
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-08-18 17:12:02 +0100

    adding code to main to test entity ranking bm25+rm3 on auto TREC CAR

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     printing_step = 50
     # run_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity_bm25_rm3_1000.run'
     # topics_path = '/nfs/trec_car/data/entity_ranking/testY2_automatic_entity_data/testY2_automatic_entity.topics'
-    run_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_bm25_rm3_1000_2.run'
+    run_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity_bm25_rm3_1000.run.v2'
     topics_path = '/nfs/trec_car/data/entity_ranking/testY2_manual_entity_data/testY2_manual_entity.topics'
     search_tools.write_run_car(topics_path, run_path, hits=hits, printing_step=printing_step)
 
