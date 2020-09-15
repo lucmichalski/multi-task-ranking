6d40797b1db077c9af8e0d58a1a2024bea4b9d21
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-15 22:35:38 +0100

    added simple BERT CLS model to learning

--- main_multi_task.py
+++ main_multi_task.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
 
     #create_extra_queries(dir_path='/nfs/trec_car/data/entity_ranking/multi_task_data/')
 
-    MultiTaskDataset().cls_processing(dir_path=dir_path, batch_size=64)
+    MultiTaskDataset().cls_processing(dir_path=dir_path, batch_size=64*3)
