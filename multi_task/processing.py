b2aa734847438b2aaaae498bfe1cc2975e49236c
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-17 16:03:19 +0100

    starting to play with data

--- multi_task/processing.py
+++ multi_task/processing.py
@@ -326,8 +326,8 @@ class MultiTaskDataset():
     def get_query_specific_data(self, dir_path, dataset_name):
         """ """
         # Content entity data
-        print('here')
         entity_content_path = self.__get_content_dir_path(dir_path=dir_path, dataset_name=dataset_name)
+        print('reading parquet file: {}'.format(entity_content_path))
         df_entity_content = pd.read_parquet(entity_content_path)
         print(df_entity_content.head())
 
