fcfff0e75eec307091a26d0b7585622639fccf66
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-21 20:36:00 +0100

    re-factoring CLS pipeline

--- main_multi_task.py
+++ main_multi_task.py
@@ -2,5 +2,5 @@
 from multi_task.processing import MultiTaskDatasetByQuery
 
 if __name__ == '__main__':
-    dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_new/'
+    dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
     MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path)
