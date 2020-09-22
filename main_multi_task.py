d387c8a76854898cfdf5ab183a9609302aeb6f02
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-22 18:52:46 +0100

    starting multi task CLS models

--- main_multi_task.py
+++ main_multi_task.py
@@ -1,6 +1,10 @@
 
 from multi_task.processing import MultiTaskDatasetByQuery
+from multi_task.ranking import rerank_runs
 
 if __name__ == '__main__':
-    dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
-    MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path)
+    # dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
+    # MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path)
+
+    dataset = 'test'
+    rerank_runs(dataset=dataset)
