c8b130f7ba08f8fd3b1078565d0f7b760e3961a7
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-22 18:51:34 +0100

    starting multi task CLS models

--- multi_task/ranking.py
+++ multi_task/ranking.py
@@ -3,6 +3,9 @@ from scipy.spatial import distance
 import json
 import os
 
+from multi_task.processing import dataset_metadata
+from retrieval.tools import EvalTools
+
 
 def get_dict_from_json(path):
     """"""
@@ -10,19 +13,24 @@ def get_dict_from_json(path):
         d = json.load(f)
     return d
 
-def write_run_to_file(query, run_data, path, how):
+
+def write_run_to_file(query, run_data, run_path, how):
     """ """
     run_data.sort(key=lambda tup: tup[1], reverse=True)
     rank = 1
-    with open(path, 'a+') as f:
+    with open(run_path, 'a+') as f:
         for doc_id, score in run_data:
             f.write(' '.join(query, 'Q0', doc_id, rank, score, how) + '\n')
             rank += 1
 
-            
+
 def get_read_data(dataset, how='euclidean', parent_dir_path='/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'):
     """ """
     dir_path = parent_dir_path + '{}_data/'.format(dataset)
+
+    passage_qrels = dataset_metadata['passage_' + dataset][1]
+    entity_qrels = dataset_metadata['entity_' + dataset][1]
+
 
     entity_links_path = dir_path + 'passage_to_entity.json'
     entity_links_dict = get_dict_from_json(path=entity_links_path)
@@ -33,16 +41,17 @@ def get_read_data(dataset, how='euclidean', parent_dir_path='/nfs/trec_car/data/
 
         query_cls = query_dict['query']['cls_token']
 
-        run = []
+        run_data = []
         for doc_id in query_dict['passage'].keys():
             doc_cls = query_dict['passage'][doc_id]['cls_token']
 
             if how == 'euclidean' :
                 score = - distance.euclidean(query_cls,  doc_cls)
-                path = dir_path + how + '_passage.run'
+                run_path = dir_path + how + '_passage.run'
             else:
                 raise
 
-            run.append((doc_id, score))
+            run_data.append((doc_id, score))
 
-        write_run_to_file(query=query, run=run, path=path, how=how)
+        write_run_to_file(query=query, run_data=run_data, run_path=run_path, how=how)
+        EvalTools().write_eval_from_qrels_and_run(qrels_path=passage_qrels, run_path=run_path)
