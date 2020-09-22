98f5eb9e500b1f7a1f02a25c7a4e352a152ddcfb
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-22 11:10:50 +0100

    building passage to entity mappings

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from multi_task.processing import MultiTaskDatasetByQuery
 
 if __name__ == '__main__':
     dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query_1000/'
-    MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path, max_rank=100)
+    MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path, max_rank=1000)
 
     #dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_new/'
     #
