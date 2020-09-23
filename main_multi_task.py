f0455336dc677678ae9f6b5ca19dd288b14a3a8d
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-23 20:34:10 +0100

    news multi-task lower lr

--- main_multi_task.py
+++ main_multi_task.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     # dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
     # MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path)
     #
-    # dataset = 'test'
-    # rerank_runs(dataset=dataset)
+    dataset = 'test'
+    rerank_runs(dataset=dataset)
 
-    train_model()
+    #train_model()
