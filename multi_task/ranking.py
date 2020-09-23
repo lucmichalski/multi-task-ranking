be06098ad51bbe3a6b3742a8aaaf83c61f48f500
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-23 19:45:45 +0100

    news multi-task lower lr

--- multi_task/ranking.py
+++ multi_task/ranking.py
@@ -89,7 +89,7 @@ def rerank_runs(dataset,  parent_dir_path='/nfs/trec_car/data/entity_ranking/mul
         EvalTools().write_eval_from_qrels_and_run(qrels_path=entity_qrels, run_path=entity_run_path)
 
 
-def train_model(batch_size=64, lr=0.005, parent_dir_path='/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'):
+def train_model(batch_size=64, lr=0.001, parent_dir_path='/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'):
     """ """
     train_dir_path = parent_dir_path + 'train_data/'
     dev_dir_path = parent_dir_path + 'dev_data/'
