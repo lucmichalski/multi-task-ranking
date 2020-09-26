9015674dd484f5055fb005f810c2cf0c390951a2
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-26 22:25:13 +0100

    ready to run on 1000 queries to see if improves

--- multi_task/ranking.py
+++ multi_task/ranking.py
@@ -806,9 +806,8 @@ def train_cls_model_max_combo(batch_size=256, lr=0.0005, parent_dir_path='/nfs/t
             if (topic_query != None) and (topic_query != query):
                 topic_run_data.sort(key=lambda x: x[1], reverse=True)
                 topic_run = [i[0] for i in topic_run_data]
-                mylist = [i[0] for i in topic_run_data]
 
-                assert len(topic_run_data) <= 101, "{} len {}: {}".format(topic_query, len(topic_run_data), [i for i in mylist if mylist.count(i) > 1])
+                #assert len(topic_run_data) <= 101, "{} len {}: {}".format(topic_query, len(topic_run_data), [i for i in topic_run if topic_run.count(i) > 1])
 
                 with open(test_run_path, 'a+') as f:
                     rank = 1
