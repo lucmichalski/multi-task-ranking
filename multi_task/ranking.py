15a1f5f3d671bac00f093aae2709a32b683a0b95
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-29 19:45:25 +0100

    stats on new cls tokens

--- multi_task/ranking.py
+++ multi_task/ranking.py
@@ -96,7 +96,7 @@ def rerank_runs(dataset,  parent_dir_path='/nfs/trec_car/data/entity_ranking/mul
         EvalTools().write_eval_from_qrels_and_run(qrels_path=entity_qrels, run_path=entity_run_path)
 
 
-def train_cls_model(batch_size=256, lr=0.0001, parent_dir_path='/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/',
+def train_cls_model(batch_size=256, lr=0.005, parent_dir_path='/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/',
                     max_rank=100):
     """ """
     train_dir_path = parent_dir_path + 'train_data/'
