ad17db8e8bd5e8b384b8788c2c9c71a88063fa24
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-09-29 16:23:59 +0100

    stats on new cls tokens

--- main_multi_task.py
+++ main_multi_task.py
@@ -4,20 +4,20 @@ from multi_task.ranking import rerank_runs, train_cls_model_max_combo, train_cls
 
 if __name__ == '__main__':
     dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
-    passage_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_passage_100_lr_8e6_num_warmup_steps_1000/epoch1_batch14000/'
-    entity_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/full_data_v2_hierarchical_10000_hits_300_v2_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch420000/'
-    MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path,
-                                                     max_rank=100,
-                                                     batch_size=64,
-                                                     bi_encode=True,
-                                                     passage_model_path=passage_model_path,
-                                                     entity_model_path=entity_model_path
-                                                     )
+    # passage_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_passage_100_lr_8e6_num_warmup_steps_1000/epoch1_batch14000/'
+    # entity_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/full_data_v2_hierarchical_10000_hits_300_v2_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch420000/'
+    # MultiTaskDatasetByQuery().build_dataset_by_query(dir_path=dir_path,
+    #                                                  max_rank=100,
+    #                                                  batch_size=64,
+    #                                                  bi_encode=True,
+    #                                                  passage_model_path=passage_model_path,
+    #                                                  entity_model_path=entity_model_path
+    #                                                  )
 
     dataset = 'test'
     rerank_runs(dataset=dataset)
 
-    train_cls_model(bi_encode=False)
-    train_cls_model(bi_encode=True)
-    train_cls_model_max_combo()
+    # train_cls_model(bi_encode=False)
+    # train_cls_model(bi_encode=True)
+    # train_cls_model_max_combo()
 
