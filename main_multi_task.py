bb57d27016c98f724aa8e7eb53cbf18a9288ed9e
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-07 18:08:50 +0100

    train remaining entity folds news keyword

--- main_multi_task.py
+++ main_multi_task.py
@@ -6,9 +6,11 @@ if __name__ == '__main__':
     dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
     passage_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_passage_100_lr_8e6_num_warmup_steps_1000/epoch1_batch14000/'
     entity_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/full_data_v2_hierarchical_10000_hits_300_v2_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch420000/'
+    batch_size = 64*6
+    max_rank = 100
     MultiTaskDatasetByQuery().build_dataset_by_query_entity_context(dir_path=dir_path,
-                                                                    max_rank=100,
-                                                                    batch_size=64,
+                                                                    max_rank=max_rank,
+                                                                    batch_size=batch_size,
                                                                     passage_model_path=passage_model_path,
                                                                     entity_model_path=entity_model_path
                                                                     )
