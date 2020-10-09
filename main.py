b5f0297c6cce64f89ce0352506d484daee95fd8c
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-09 21:28:02 +0100

    run train max ent rank

--- main.py
+++ main.py
@@ -12,18 +12,26 @@ from multi_task.processing import MultiTaskDatasetByQuery
 
 
 if __name__ == '__main__':
+    from multi_task.ranking import train_cls_model_max_combo, train_cls_model, train_mutant_max_combo
 
-    dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query_1000/'
-    passage_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_passage_100_lr_8e6_num_warmup_steps_1000/epoch1_batch14000/'
-    entity_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/full_data_v2_hierarchical_10000_hits_300_v2_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch420000/'
-    batch_size = 64*6
-    max_rank = 1000
-    MultiTaskDatasetByQuery().build_dataset_by_query_entity_context(dir_path=dir_path,
-                                                                    max_rank=max_rank,
-                                                                    batch_size=batch_size,
-                                                                    passage_model_path=passage_model_path,
-                                                                    entity_model_path=entity_model_path
-                                                                    )
+    batch_sizes = [64, 256]
+    lrs = [0.00001, 0.0001, 0.0005, 0.001]
+    for batch_size in batch_sizes:
+        for lr in lrs:
+    #         train_cls_model(batch_size=batch_size, lr=lr)
+            train_mutant_max_combo(batch_size=batch_size, lr=lr)
+
+    # dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query_1000/'
+    # passage_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/benchmarkY1_passage_100_lr_8e6_num_warmup_steps_1000/epoch1_batch14000/'
+    # entity_model_path = '/nfs/trec_car/data/bert_reranker_datasets/exp/full_data_v2_hierarchical_10000_hits_300_v2_lr_2e6_num_warmup_steps_0.1_new_pipeline/epoch1_batch420000/'
+    # batch_size = 64*6
+    # max_rank = 1000
+    # MultiTaskDatasetByQuery().build_dataset_by_query_entity_context(dir_path=dir_path,
+    #                                                                 max_rank=max_rank,
+    #                                                                 batch_size=batch_size,
+    #                                                                 passage_model_path=passage_model_path,
+    #                                                                 entity_model_path=entity_model_path
+    #                                                                 )
     # from multi_task.ranking import rerank_runs, train_cls_model_max_combo, train_cls_model
     #
     # dir_path = '/nfs/trec_car/data/entity_ranking/multi_task_data_by_query/'
