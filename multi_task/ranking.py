5f847cdfaebf86093871617193b1637788762452
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-10 16:38:14 +0100

    inference keyoword BERT

--- multi_task/ranking.py
+++ multi_task/ranking.py
@@ -1467,6 +1467,8 @@ def train_mutant_multi_task_max_combo(batch_size=128, lr=0.0001, parent_dir_path
             inputs, labels = train_batch
             passage_output, entity_output = model.forward(inputs.to(device))
             print('inputs: {}, labels: {}'.format(inputs.shape, labels.shape))
+            print('passage_output: {}, entity_output: {}'.format(passage_output.shape, entity_output.shape))
+
             print(labels[0], labels[1])
 
 
