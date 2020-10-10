d26a6d5807640031608538db60ef531f47da61ec
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-10 16:57:06 +0100

    inference keyoword BERT

--- multi_task/ranking.py
+++ multi_task/ranking.py
@@ -1470,13 +1470,14 @@ def train_mutant_multi_task_max_combo(batch_size=128, lr=0.0001, parent_dir_path
             #print('passage_output: {}, entity_output: {}'.format(passage_output.shape, entity_output.shape))
             print('--------')
             print(labels[:,0].reshape(-1))
-            print(passage_output.cpu())
+            print(passage_output.reshape(-1).cpu())
 
 
             # Calculate Loss: softmax --> cross entropy loss
-            passage_loss = loss_func(passage_output.cpu(), labels[:,0].reshape(-1))
-            entity_loss = loss_func(entity_output.cpu(), labels[:,1].reshape(-1))
+            passage_loss = loss_func(passage_output.reshape(-1).cpu(), labels[:,0].reshape(-1))
+            entity_loss = loss_func(entity_output.reshape(-1).cpu(), labels[:,1].reshape(-1))
             loss = passage_loss + entity_loss
+            print(loss)
             # Getting gradients w.r.t. parameters
             loss.sum().backward()
             torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
