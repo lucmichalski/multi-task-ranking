39bff48edeac0aa02077c546390d0f763ba8d997
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-14 05:02:37 +0100

    running mutant max on news

--- main_multi_task.py
+++ main_multi_task.py
@@ -1,19 +1,20 @@
 
 from multi_task.processing import MultiTaskDatasetByQuery
-from multi_task.ranking import  train_cls_model_max_combo, train_cls_model, train_mutant_max_combo, train_mutant_multi_task_max_combo
+from multi_task.ranking import train_cls_model_max_combo, train_cls_model, train_mutant_max_combo, \
+    train_mutant_multi_task_max_combo, train_mutant_multi_task_max_combo_news
 
 if __name__ == '__main__':
-    dir_path = '/nfs/trec_news_track/data/5_fold/'
-    passage_model_path = '/nfs/trec_news_track/data/5_fold/scaled_5fold_0_data/exp/passage_ranking_bert_train_2epoch+8e-06lr/epoch1_batch2000/'
-    entity_model_path = '/nfs/trec_news_track/data/5_fold/scaled_5fold_0_data/exp/entity_ranking_bert_train_2epoch+8e-06lr/epoch2_batch6000/'
-    batch_size = 64*2
-    max_rank = 100
-    MultiTaskDatasetByQuery().build_dataset_by_query_entity_context_news(dir_path=dir_path,
-                                                                        max_rank=max_rank,
-                                                                        batch_size=batch_size,
-                                                                        passage_model_path=passage_model_path,
-                                                                        entity_model_path=entity_model_path
-                                                                        )
+    # dir_path = '/nfs/trec_news_track/data/5_fold/'
+    # passage_model_path = '/nfs/trec_news_track/data/5_fold/scaled_5fold_0_data/exp/passage_ranking_bert_train_2epoch+8e-06lr/epoch1_batch2000/'
+    # entity_model_path = '/nfs/trec_news_track/data/5_fold/scaled_5fold_0_data/exp/entity_ranking_bert_train_2epoch+8e-06lr/epoch2_batch6000/'
+    # batch_size = 64*2
+    # max_rank = 100
+    # MultiTaskDatasetByQuery().build_dataset_by_query_entity_context_news(dir_path=dir_path,
+    #                                                                     max_rank=max_rank,
+    #                                                                     batch_size=batch_size,
+    #                                                                     passage_model_path=passage_model_path,
+    #                                                                     entity_model_path=entity_model_path
+    #                                                                     )
 
     # train_mutant_max_combo()
 
@@ -21,7 +22,10 @@ if __name__ == '__main__':
     # lrs = [0.000001, 0.00001, 0.0001, 0.0005, 0.001]
     # for batch_size in batch_sizes:
     #     for lr in lrs:
-    #         train_mutant_multi_task_max_combo(batch_size=batch_size, lr=lr, mutant_type='mean')
+    #         train_mutant_multi_task_max_combo_news(batch_size=batch_size, lr=lr, mutant_type='mean')
+    batch_size = 64
+    lr = 0.0001
+    train_mutant_multi_task_max_combo_news(batch_size=batch_size, lr=lr, mutant_type='max')
 
 
 
