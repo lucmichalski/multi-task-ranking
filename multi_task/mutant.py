e5e285330293100252a5dd78deb2c7c9df494969
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-15 22:55:51 +0100

    test mutant train and dev

--- multi_task/mutant.py
+++ multi_task/mutant.py
@@ -326,8 +326,8 @@ def train_and_dev_mutant(dev_save_path_run, dev_save_path_dataset, train_save_pa
                     loss = loss_func(outputs.cpu(), labels)
 
                     dev_loss_total += loss.sum().item()
-                    dev_scores += list(itertools.chain(*outputs.permute(1, 0, 2).cpu().numpy().tolist()))
-                    dev_labels += list(itertools.chain(*labels.permute(1, 0, 2).cpu().numpy().tolist()))
+                    dev_scores += list(itertools.chain(*outputs.cpu().numpy().tolist()))
+                    dev_labels += list(itertools.chain(*labels.cpu().numpy().tolist()))
 
                 unpacked_dev_run_data = unpack_run_data(dev_run_data, max_seq_len=max_seq_len)
 
@@ -335,5 +335,5 @@ def train_and_dev_mutant(dev_save_path_run, dev_save_path_dataset, train_save_pa
                 print('dev loss @ step {}, {}'.format(i_train, dev_loss_total / (len(dev_data_loader) + 1)))
                 for dev_label, dev_score, unpack_run in zip(dev_labels, dev_scores, unpacked_dev_run_data):
                     task, query, doc_id, label = unpack_run
-                    assert dev_label == label
+                    assert dev_label == label, '{} == {}'.format(dev_label, label)
 
