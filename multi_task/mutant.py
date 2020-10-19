ab35eadf2feebd28e948c0dc058ebabd56993b8b
Iain Mackie <iain@Iains-MacBook-Pro.local>
2020-10-19 20:35:51 +0100

    rounding assertion

--- multi_task/mutant.py
+++ multi_task/mutant.py
@@ -335,7 +335,7 @@ def train_and_dev_mutant(dev_save_path_run, dev_save_path_dataset, train_save_pa
                 print('dev loss @ step {}, {}'.format(i_train, dev_loss_total / (len(dev_data_loader) + 1)))
                 for dev_label, dev_score, unpack_run in zip(dev_labels, dev_scores, unpacked_dev_run_data):
                     task, query, doc_id, label = unpack_run[0], unpack_run[1], unpack_run[2], unpack_run[3]
-                    assert dev_label[0] == label, '{} == {}'.format(dev_label[0], label)
+                    assert round(dev_label[0], 2) == round(label, 2), '{} == {}'.format(dev_label[0], label)
                     print(task, query, doc_id, label, dev_label[0], dev_score)
 
 
